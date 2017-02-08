af458bb0c9322d62565ea7c5676e8eb502b42a9c
K5680 <k5680@student.jamk.fi>
2017-02-08 11:07:19 +0200

    1.0

---
+++ labra3_teht4/labra3_teht4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labra3_teht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle datsun = new Vehicle();
+            datsun.Name = "Jukkis";
+            datsun.Speed = 79;
+            datsun.Tyres = 4;
+
+            datsun.PrintData();
+
+            System.Console.ReadLine();
+            
+            datsun.Speed = 45;
+            Console.WriteLine(datsun.ToString());
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
---
+++ labra3_teht4/labra3_teht4/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labra3_teht4
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Car Name : " + Name);
+            Console.WriteLine("Speed : " + Speed);
+            Console.WriteLine("Tyres: " + Tyres);
+        }
+
+        public override string ToString()
+        {
+            return "Name:" + Name + " Speed: " + Speed + " Tyres: " + Tyres;
+        }
+    }
+}
