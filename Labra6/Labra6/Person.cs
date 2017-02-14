aa5c1dee1e5d515f253e4b42e7dce13dd151291f
K5680 <k5680@student.jamk.fi>
2017-02-14 22:38:14 +0200

    v1

---
+++ Labra6/Labra6/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra6
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Nro { get; set; }
+
+
+        public Person()
+        {
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Nro;
+        }
+
+    }
+}
---
+++ Labra6/Labra6/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Labra6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int luotavien_maara = 10000;
+            int nro = 0;
+            int etunimen_pituus = 4;
+            int sukunimen_pituus = 10;
+
+            //arvonnan alustus
+            var etunimi = new char[etunimen_pituus];
+            var sukunimi = new char[sukunimen_pituus];
+            var kirjaimet = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+            var random = new Random();
+
+            //luodaan lista
+            List<Person> lista = new List<Person>();
+            
+            // kellotus
+            Stopwatch kello = new Stopwatch();
+            kello.Start();
+
+            do
+            {
+                nro++;
+                //henkilöjen luonti
+                for (int i = 0; i < etunimen_pituus; i++)
+                {
+                    etunimi[i] = kirjaimet[random.Next(kirjaimet.Length)];
+                }
+                var etu = new string(etunimi);
+
+                for (int i = 0; i < sukunimen_pituus; i++)
+                {
+                    sukunimi[i] = kirjaimet[random.Next(kirjaimet.Length)];
+                }
+
+                var suku = new string(sukunimi);
+
+                //lisätään henkilö listaan
+                lista.Add(new Person { FirstName = etu, LastName = suku, Nro = nro });//etu, suku, nro));
+                 
+            } while (nro < luotavien_maara);
+            //kellon pysäytys
+            kello.Stop();
+            Console.WriteLine("Tietojen luomiseen kulunut aika: " + kello.Elapsed + " (enter");
+            Console.ReadKey();
+
+
+            //tietojen näyttäminen          
+            //          foreach (Person person in lista)
+            //          {
+            //                Console.WriteLine(person.ToString());
+            //          }
+
+            //etunimi jota etsitään, hylätty versio
+            //var etsi = new string(etunimi);
+            //Find Using Inline
+            //Person em = lista.Find(delegate (Person e) { return e.FirstName == etsi; });
+            //Console.WriteLine("etsitty: " + em);
+
+            string apustring;
+            //arvotaan etsittävä sana
+            for (int i = 0; i < etunimen_pituus; i++)
+            {
+                etunimi[i] = kirjaimet[random.Next(kirjaimet.Length)];
+            }
+            string etu2 = new string(etunimi);
+
+            //etsitään sanaa
+            Console.WriteLine("Etsitään..." +etu2);
+            for (int i = 0; i < luotavien_maara; i++)
+            {
+                apustring = lista[i].ToString();
+                if (apustring.Contains(etu2) == true )
+                {
+                    Console.Write("\nLöytyi: " + lista[i]);
+                    
+                }
+                else
+                {
+                    Console.Write("etsitään: " + etu2 + " " +lista[i] +  "\n");
+                }
+
+            }
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}
