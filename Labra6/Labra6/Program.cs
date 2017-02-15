cb5eff606c7b5797b7badbf36c45fcd66b6fbedb
K5680 <k5680@student.jamk.fi>
2017-02-15 10:51:41 +0200

    v3

--- Labra6/Labra6/Program.cs
+++ Labra6/Labra6/Program.cs
@@ -69,6 +69,7 @@ namespace Labra6
             string apustring;
             string etu2;
             double match = 0; // luotu doublena, jotta prosenttilasku toimii desimaalilla
+            
             for (int tuhat = 0; tuhat < 1000; tuhat++)
             {
 
@@ -83,16 +84,12 @@ namespace Labra6
                 //Console.WriteLine("Etsitään nimeä:  " + etu2);
                 for (int i = 0; i < luotavien_maara; i++)
                 {
-                    apustring = lista[i].ToString();
+                    apustring = lista[i].FirstName.ToString();// jos etu2 -stringi löytyy listan FirstName -sarakkeesta
                     if (apustring.Contains(etu2) == true)
                     {
                         Console.Write(".");
-                        //             Console.Write("\n{0} <MATCH> -> {1}", etu2, lista[i]);
+//                        Console.Write("\n{0} <MATCH> -> {1}", etu2, lista[i]);
                         match++;
-                    }
-                    else
-                    {
-                         
                     }
 
                 }
@@ -102,7 +99,72 @@ namespace Labra6
             Console.WriteLine("\nOsumia " + match + " / " + luotavien_maara + " = " + (match/System.Convert.ToDouble(luotavien_maara))*100 +  "%");
             Console.ReadKey();
 
+            kello.Reset();
+            // Tehdään sama Dictionary-tyypillä
+            kello.Start();
+            Dictionary<string, Person> persons = new Dictionary<string, Person>();
+            nro = 0;
+            do
+            {
+                nro++;
+                //henkilöjen luonti
+                for (int i = 0; i < etunimen_pituus; i++)
+                {
+                    etunimi[i] = kirjaimet[random.Next(kirjaimet.Length)];
+                }
+                var etu = new string(etunimi);
+
+                for (int i = 0; i < sukunimen_pituus; i++)
+                {
+                    sukunimi[i] = kirjaimet[random.Next(kirjaimet.Length)];
+                }
+
+                var suku = new string(sukunimi);
+
+                //lisätään henkilö listaan
+
+                string nim = "nro"+nro;
+                persons.Add(nim, new Person { FirstName = etu, LastName = suku, Nro = nro });//etu, suku, nro));
+                
+            } while (nro < luotavien_maara);
+            kello.Stop();
+            Console.WriteLine("Tietoja luotu: " + nro);
+            Console.WriteLine("Tietojen luomiseen kulunut aika: " + kello.Elapsed + " <enter>");
+            Console.ReadKey();
+
+
+            kello.Reset();
+            kello.Start();
+            //etsitään tuhatta eri "nimeä" Dictionarystä
+            match = 0;
+            for (int tuhat = 0; tuhat < 1000; tuhat++)
+            {
+                //arvotaan etsittävä sana
+                for (int i = 0; i < etunimen_pituus; i++)
+                {
+                    etunimi[i] = kirjaimet[random.Next(kirjaimet.Length)];
+                }
+                etu2 = new string(etunimi);
+
+                // jos etu2 -stringi löytyy Dictionaryn FirstName -sarakkeesta
+                foreach (Person person in persons.Values)
+                {
+                    if (person.FirstName == etu2)
+                    {
+                        Console.Write(".");
+                        match++;
+                    }
+                    
+                }
+            }
+
+            kello.Stop();
+            Console.WriteLine("\n\nEtsimiseen kulunut aika: " + kello.Elapsed + "   Pareja löydetty: " + match + " <enter>");
+            Console.WriteLine("\nOsumia " + match + " / " + luotavien_maara + " = " + (match / System.Convert.ToDouble(luotavien_maara)) * 100 + "%");
             
+
+            Console.ReadKey();
+
         }
     }
 }
